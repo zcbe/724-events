6c17ec802ed557032b60b40b4f16238e5f203b8b
zcbe <151560157+zcbe@users.noreply.github.com>
2024-03-29 15:50:39 +0100

    Suppression erreurs console Slider : key unique, balise vide, et ajout de readOnly pour la pagination

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -17,10 +17,10 @@ const Slider = () => {
   const nextCard = () => {
     setTimeout(
       /*
-        Suppression de l'élément "undefined" en ajoutant -1 à la taille du tableau
-        Un tableau commence toujours à 0 !
+        Suppression de l'élément "undefined" en ajoutant +1 à index
+        Ajout de "?" pour vérifier que byDateDesc existe
       */
-        () => setIndex(index < byDateDesc.length -1 ? index + 1 : 0),
+        () => setIndex(index + 1 < byDateDesc?.length ? index + 1 : 0),
         5000
     );
   };
@@ -28,14 +28,12 @@ const Slider = () => {
     nextCard();
   });
   return (
-    <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-      <div>
-          <div
-            /*
-              Modification de key pour que chaque image est un ID unique
-            */
-              key={event.id}
+    <div className="SlideCardList">   
+      {/* Suppresion des <></> qui encapsulait 2 éléments différents */}   
+            {byDateDesc?.map((event, idx) => (
+           // Changement de la key pour qu'elle soit unique pour chaque slide
+           <div key={event.date}>
+           <div 
               className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
@@ -54,12 +52,16 @@ const Slider = () => {
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  // Changement de la key pour qu'elle corresponde à la slide en cours
+                  key={_.date}
                   type="radio"
                   name="radio-button"
                  /* Remplacement de idx par index pour indiquer sur quelle image on se trouve  */
-                  checked={index === radioIdx}                />
-              ))}
+                  checked={index === radioIdx}       
+                // Ajout de readOnly pour retirer erreur console
+                readOnly
+                />
+                ))}
             </div>
           </div>
         </div>
