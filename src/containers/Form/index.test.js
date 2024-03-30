6b52e1b5c43e99c834fbedcb5ea7c46ea374e354
zcbe <151560157+zcbe@users.noreply.github.com>
2024-03-30 11:05:26 +0100

    Correction bugs formulaire

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -1,8 +1,8 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Form from "./index";
 
-describe("When Events is created", () => {
-  it("a list of event card is displayed", async () => {
+describe("When Form is created", () => {
+  it("a list of input is displayed", async () => {
     render(<Form />);
     await screen.findByText("Email");
     await screen.findByText("Nom");
@@ -22,7 +22,8 @@ describe("When Events is created", () => {
         })
       );
       await screen.findByText("En cours");
-      await screen.findByText("Envoyer");
+      // On attend que le "Message envoyé" apparaisse
+      await waitFor(() => screen.findByText("Message envoyé !"), {timeout:2000});
       expect(onSuccess).toHaveBeenCalled();
     });
   });
